22098ab7eb3917a848c9933fd49d34e07cedc6fd
annause <73198650+annause@users.noreply.github.com>
2025-03-22 15:01:16 -0600

    src commit
    src commit

---
+++ Front/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DoctorRegistration from "./components/DoctorRegistration";
+
+function App() {
+  return (
+    <div>
+      <h1></h1>
+      <DoctorRegistration />
+    </div>
+  );
+}
+
+export default App;
